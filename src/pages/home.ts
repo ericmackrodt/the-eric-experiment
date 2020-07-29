6344da1babb5078149bb76e4e8c4d1935b9ca42f
Eric Mackrodt <eric.mackrodt@safetyculture.io>
2020-07-29 10:59:27 +1000

    Intro

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
-import { viewPath } from "../view-path";
+import * as fs from "fs";
+import * as marked from "marked";
+import * as path from "path";
 import { BlogData } from "../types";
+import { viewPath } from "../view-path";
 
 export function home(blogData: BlogData) {
   return async (req: Request, res: Response) => {
-    res.render(viewPath(req, "home"), { ...blogData, showSideContent: true });
+    const filePath = path.join(__dirname, "../../contents/intro.md");
+    const content = fs.readFileSync(filePath).toString("utf-8");
+    const intro = marked(content, {});
+    res.render(viewPath(req, "home"), {
+      ...blogData,
+      showSideContent: true,
+      intro,
+    });
   };
 }
