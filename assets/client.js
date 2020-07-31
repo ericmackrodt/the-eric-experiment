8f01ce5e2a71cce79db476b8bdd17aaae236511b
Eric Mackrodt <eric.mackrodt@safetyculture.io>
2020-07-31 11:24:07 +1000

    Mobile menu

--- assets/client.js
+++ assets/client.js
@@ -101,6 +101,46 @@ Vue.component("desktop", {
   },
 });
 
+Vue.directive("click-outside", {
+  bind: function (el, binding, vnode) {
+    this.event = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener("click", this.event);
+  },
+  unbind: function (el) {
+    document.body.removeEventListener("click", this.event);
+  },
+});
+
+Vue.component("mobileMenu", {
+  template: `
+    <div class="mobile-friendly-menu" v-click-outside="closeMenu">
+      <a class="menuitem mobile" v-on:click="toggleMenu" v-bind:class="{ active: isOpen }">
+        Menu
+      </a>
+      <div class="main-menu-items" v-bind:class="{ open: isOpen }">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+    closeMenu() {
+      this.isOpen = false;
+    },
+  },
+});
+
 const vueApp = new Vue({
   el: "#root",
 });
